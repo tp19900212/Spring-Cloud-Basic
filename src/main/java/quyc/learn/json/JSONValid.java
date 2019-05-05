a64a76808027025ba5a569e53fd8b21dfebec5db
andi <qyc13598009904>
2019-05-05 15:44:48 +0800

    JSON格式化校验

---
+++ src/main/java/quyc/learn/json/JSONValid.java
@@ -0,0 +1,27 @@
+package quyc.learn.json;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Arrays;
+
+/**
+ * @author: andy
+ * @create: 2019/4/24 11:26
+ * @description: json格式校验
+ */
+public class JSONValid {
+
+    public static void main(String[] args) {
+        JSONObject object = new JSONObject();
+        object.put("aa", Arrays.asList(1, 2, 3, 4, 5));
+        object.put("ss", 1);
+        object.put("vvv", "String");
+        String str = object.toJSONString();
+        System.out.println("str = " + str);
+        boolean valid = JSONObject.isValid(str);
+        System.out.println("valid = " + valid);
+        String str1 = "aall|abc|ddd";
+        System.out.println(JSONObject.isValid(str1));
+
+    }
+}
