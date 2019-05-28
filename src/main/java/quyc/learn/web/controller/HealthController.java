e43360cdff4768182781c689e7948434c1a3291b
quyc07 <quyc07@163.com>
2019-05-28 12:08:00 +0800

    health check

---
+++ src/main/java/quyc/learn/web/controller/HealthController.java
@@ -0,0 +1,18 @@
+package quyc.learn.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: andy
+ * @create: 2019/5/28 12:06
+ * @description: check health
+ */
+@RestController
+public class HealthController {
+
+    @RequestMapping("/checkHealth")
+    public String checkHealth() {
+        return "success";
+    }
+}
