506ea1fdcd0c80492c1f7f91c75714a45daf54db
quyc07 <quyc07@163.com>
2019-06-10 11:17:06 +0800

    项目结构调整，zipkin独立工程部署

--- zipkin/src/main/java/com/quyc/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/quyc/zipkin/ZipkinApplication.java
@@ -2,11 +2,9 @@ package com.quyc.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZipkinServer
 public class ZipkinApplication {
 
