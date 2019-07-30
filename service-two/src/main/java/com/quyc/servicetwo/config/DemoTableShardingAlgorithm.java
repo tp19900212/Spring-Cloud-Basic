133b8a02faa3c5b25c2642899596ee7fbcb4f9bd
quyc07 <quyc07@163.com>
2019-07-31 00:07:52 +0800

    集成sharding-JDBC实现分库分表（未完成）

---
+++ service-two/src/main/java/com/quyc/servicetwo/config/DemoTableShardingAlgorithm.java
@@ -0,0 +1,24 @@
+package com.quyc.servicetwo.config;
+
+import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+import java.util.Collection;
+
+/**
+ * @author quyuanchao
+ * @date 2019/7/30 23:41
+ * <p>Title: </p>
+ * <p>Description: 分片策略</p>
+ */
+public class DemoTableShardingAlgorithm implements PreciseShardingAlgorithm<Long> {
+    @Override
+    public String doSharding(Collection<String> collection, PreciseShardingValue<Long> preciseShardingValue) {
+        for (String each : collection) {
+            if (each.endsWith(Long.parseLong(preciseShardingValue.getValue().toString()) % 2 + "")) {
+                return each;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
