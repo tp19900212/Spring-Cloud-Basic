4c5ff3566124489cf9d8d5d4f6e4e97f1f3bb4b8
quyc07 <quyc07@163.com>
2019-09-10 17:28:45 +0800

    scroll slice学习

--- learn/src/main/java/com/quyc/learn/es/search/SearchScrollApi.java
+++ learn/src/main/java/com/quyc/learn/es/search/SearchScrollApi.java
@@ -1,5 +1,6 @@
 package com.quyc.learn.es.search;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
 import com.quyc.learn.es.EsClientUtil;
 import com.quyc.learn.es.entity.Person;
@@ -13,6 +14,7 @@ import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.slice.SliceBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
@@ -29,8 +31,8 @@ public class SearchScrollApi {
     private static RestHighLevelClient client = EsClientUtil.getClient();
 
     public static void main(String[] args) throws IOException {
-        searchScroll();
-//        test1();
+//        searchScroll();
+        searchScrollSliace();
     }
 
     public static void searchScroll() throws IOException {
@@ -54,6 +56,8 @@ public class SearchScrollApi {
             // 获取scrollId，后续搜索根据这个id进行搜索
             scrollId = searchResponse.getScrollId();
             System.out.println("scrollId = " + scrollId);
+            List<Person> searchResult = EsClientUtil.getSearchResult(searchResponse, Person.class);
+            System.out.println("searchResult = " + searchResult);
             SearchHit[] hits = searchResponse.getHits().getHits();
             // 循环搜索，直到搜索结束
             while (hits != null && hits.length > 0) {
@@ -62,7 +66,7 @@ public class SearchScrollApi {
                 SearchResponse searchScrollResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
                 scrollId = searchScrollResponse.getScrollId();
                 hits = searchScrollResponse.getHits().getHits();
-                List<Person> searchResult = EsClientUtil.getSearchResult(searchScrollResponse, Person.class);
+                searchResult = EsClientUtil.getSearchResult(searchScrollResponse, Person.class);
                 System.out.println("searchResult = " + searchResult);
             }
             // 清理scroll上下文，若不及时清理会在时效到期自动清除
@@ -74,14 +78,43 @@ public class SearchScrollApi {
         }
     }
 
-    public static void test1() throws IOException {
-        SearchRequest searchRequest = new SearchRequest("buyer_portrait_calculate");
+    public static void searchScrollSliace() throws IOException {
+        SearchRequest searchRequest = new SearchRequest("person2");
+        Scroll scroll = new Scroll(TimeValue.timeValueSeconds(10));
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.from(100001);
-        searchSourceBuilder.size(10);
-        searchRequest.source(searchSourceBuilder);
-        SearchResponse search = client.search(searchRequest);
-        System.out.println("search = " + search);
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        int maxSlice = 4;
+        for (int id = 0; id < maxSlice; id++) {
+            searchSourceBuilder.slice(new SliceBuilder(id, maxSlice));
+            searchSourceBuilder.size(5);
+            searchRequest.source(searchSourceBuilder);
+            searchRequest.scroll(scroll);
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            // 获取scrollId，后续搜索根据这个id进行搜索
+            String scrollId = searchResponse.getScrollId();
+            System.out.println("scrollId = " + scrollId);
+            List<Person> searchResult = EsClientUtil.getSearchResult(searchResponse, Person.class);
+            System.out.println("searchResult = " + JSON.toJSONString(searchResult));
+            System.out.println("searchResult.size() = " + searchResult.size());
+            SearchHit[] hits = searchResponse.getHits().getHits();
+            // 循环搜索，直到搜索结束
+            while (hits != null && hits.length > 0) {
+                SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+                scrollRequest.scroll(scroll);
+                SearchResponse searchScrollResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+                scrollId = searchScrollResponse.getScrollId();
+                hits = searchScrollResponse.getHits().getHits();
+                searchResult = EsClientUtil.getSearchResult(searchScrollResponse, Person.class);
+                System.out.println("searchResult = " + JSON.toJSONString(searchResult));
+                System.out.println("searchResult.size() = " + searchResult.size());
+            }
+            // 清理scroll上下文，若不及时清理会在时效到期自动清除
+            ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+            clearScrollRequest.addScrollId(scrollId);
+            ClearScrollResponse clearScrollResponse = client.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+            boolean succeeded = clearScrollResponse.isSucceeded();
+            System.out.println("succeeded = " + succeeded);
+        }
     }
 
 }
