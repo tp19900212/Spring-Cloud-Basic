a378eec5475e61117514518f1a88faf0279fd74a
quyc07 <quyc07@163.com>
2019-09-17 00:34:46 +0800

    Zipkin服务优化，使用注册中心发现

--- zipkin/src/main/java/com/quyc/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/quyc/zipkin/ZipkinApplication.java
@@ -2,10 +2,12 @@ package com.quyc.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableDiscoveryClient
 public class ZipkinApplication {
 
     public static void main(String[] args) {
